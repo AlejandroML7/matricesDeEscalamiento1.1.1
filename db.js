1608c79c74c1ceaf2ffc92461fb1cd8bb3cf9061
alejandroml7 <alejandromoreno.lopez1994@gmail.com>
2023-04-10 12:27:43 -0500

    se agrego el boton de exportar pdf

---
+++ db.js
@@ -0,0 +1,148 @@
+const empresas = [
+    {
+        nombre: 'Bancolombia',
+        matriz1: [
+             {
+                nombre: 'Rodrigo Puentes',
+                numContacto: 3508886454,
+                cargo: 'gerente',
+                disponible: 'si',
+                turno: 'diurno'
+            },
+            {
+                nombre: 'Ángela-Díaz',
+                numContacto: 3128954587,
+                cargo: 'subgerente',
+                disponible: 'no',
+                turno: 'nocturno'
+            }
+        ],
+        matriz2: [
+            {
+                nombre: 'Ricardo Calderon',
+                numContacto: 3509876758,
+                cargo: 'Contador',
+                disponible: 'no',
+                turno: 'diurno'
+            },
+            {
+                nombre: 'Camilo Cifuentes',
+                numContacto: 3214566776,
+                cargo: 'Asistente contador',
+                disponible: 'si',
+                turno: 'nocturno'
+            }
+        ]
+        
+        
+    },
+    {
+    nombre: 'Colmedicos',
+    matriz1: [
+        {
+            nombre: 'Rodrigo Puentes',
+            numContacto: 3508886454,
+            cargo: 'gerente',
+            disponible: 'si',
+            turno: 'diurno'
+        },   
+       {
+            nombre: 'Ángela-Díaz',
+            numContacto: 3128954587,
+            cargo: 'subgerente',
+            disponible: 'no',
+            turno: 'nocturno'
+       }
+   ],
+   matriz2: [
+       {
+           nombre: 'Ricardo Caldearon',
+           numContacto: 3509876758,
+           cargo: 'Contador',
+           disponible: 'no',
+           turno: 'diurno'
+       },
+       {
+           nombre: 'Camilo Cifuentes',
+           numContacto: 3214566776,
+           cargo: 'Asistente contador',
+           disponible: 'si',
+           turno: 'nocturno'
+       }
+    ]
+            
+    },
+    {
+    nombre: 'Refisal',
+    matriz1: [
+        {
+           nombre: 'Rodrigo Puentes',
+           numContacto: 3508886454,
+           cargo: 'gerente',
+           disponible: 'si',
+           turno: 'diurno'
+       },
+       {
+           nombre: 'Ángela-Díaz',
+           numContacto: 3128954587,
+           cargo: 'subgerente',
+           disponible: 'no',
+           turno: 'nocturno'
+       }
+   ],
+   matriz2: [
+       {
+           nombre: 'Ricardo Caldearon',
+           numContacto: 3509876758,
+           cargo: 'Contador',
+           disponible: 'no',
+           turno: 'diurno'
+       },
+       {
+           nombre: 'Camilo Cifuentes',
+           numContacto: 3214566776,
+           cargo: 'Asistente contador',
+           disponible: 'si',
+           turno: 'nocturno'
+       }
+    ]
+
+    },
+    {
+        nombre: 'Colpatria',
+        matriz1: [
+            {
+               nombre: 'Rodrigo Puentes',
+               numContacto: 3508886454,
+               cargo: 'gerente',
+               disponible: 'si',
+               turno: 'diurno'
+           },
+           {
+               nombre: 'Ángela-Díaz',
+               numContacto: 3128954587,
+               cargo: 'subgerente',
+               disponible: 'no',
+               turno: 'nocturno'
+           }
+       ],
+       matriz2: [
+           {
+               nombre: 'Ricardo Caldearon',
+               numContacto: 3509876758,
+               cargo: 'Contador',
+               disponible: 'no',
+               turno: 'diurno'
+           },
+           {
+               nombre: 'Camilo Cifuentes',
+               numContacto: 3214566776,
+               cargo: 'Asistente contador',
+               disponible: 'si',
+               turno: 'nocturno'
+           }
+        ]
+    
+        }
+
+]
