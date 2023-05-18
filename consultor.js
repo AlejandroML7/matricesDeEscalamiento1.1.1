bca87d4ce46ee58c41fa4d0f431483213362f3f2
alejandroml7 <alejandromoreno.lopez1994@gmail.com>
2023-05-18 15:10:40 -0500

    se creo un nuevo archivo.json para extarer los tipos de matriz

--- consultor.js
+++ consultor.js
@@ -4,8 +4,79 @@ const tablaDia = document.querySelector('#diurno');
 const tablaNoche = document.querySelector('#nocturno');
 const contenedorTiposMatriz = document.querySelector('#contenedor_tipos-matriz');
 const ulListaTiposDeMatrices = document.querySelector('#lista_tipos-matrices');
+const tiposMatrices = [];
+
+// llamar a la funcion para mostrar los datos haciendo el filtro antes
+mostarJSON();
 
 
+function mostarJSON() {
+
+    //petición de los datos al API
+    const xhttp = new XMLHttpRequest();
+    xhttp.open('GET','datosClientes.json', true);
+    xhttp.send();
+    xhttp.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200){
+            
+            let datos = JSON.parse(this.responseText);
+            
+            //se crea un arreglo con todos los contactos
+            const contactos = datos.contactos;
+            
+            
+              // se recorre el array de los contactos para obtener cuantas variantes de matriz tiene el cliente
+              let valoresMatriz = new Set();
+              contactos.forEach(objeto => {
+                let tipoMatriz = objeto.matriz;
+                if (tipoMatriz) {
+                  valoresMatriz.add(tipoMatriz);
+                }
+              });
+              
+              let nuevoArray = [...valoresMatriz];
+              
+             
+              
+
+              llenarContenedorTiposMatriz(nuevoArray);
+            
+        }
+    }
+}
+
+
+function llenarContenedorTiposMatriz(arr){
+    arr.forEach(obj =>{
+        const li = document.createElement('li');
+        li.textContent = obj;
+        ulListaTiposDeMatrices.appendChild(li);
+        })
+}
+
+
+
+/**   function crearTablaDesdeObjeto(Obj) {
+                const encabezados = Object.keys(Obj);
+                const valores = Object.values(Obj);
+                
+                
+                encabezados.forEach(encabezado => {
+                  const th = document.createElement('th');
+                  th.innerText = encabezado;
+                  trHead.appendChild(th);
+                });
+            
+            
+                valores.forEach(valor => {
+                    const td = document.createElement('td');
+                    td.innerText = valor;
+                    trBody.appendChild(td);
+                  });
+            
+                
+            
+              }*/
 
 
 // eventlisteners
