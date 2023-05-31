eab39424a37eda8e1cb06ea1ec090efe78476601
alejandroml7 <alejandromoreno.lopez1994@gmail.com>
2023-05-31 15:22:50 -0500

    cambio a index.html

--- app.js
+++ app.js
@@ -7,74 +7,21 @@ const ingresarBtn = document.getElementById("ingresar");
 const form = document.querySelector('#form');
 
 
-llenarSelect(empresas);
+
 
 
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    if (accion.value == "consultor") {
-        window.location.href = "./panelConsultor.html";
-        console.log(datosIngreso);
-        return;
-        
-    } 
-    if(accion.value == "administrar"){
-        window.location.href = "./panelAmin.html";
-        return;
-    }
+    
+        window.location.href = "./panelConsultor";
+
   });
   
 
 
-selectEmpresa.addEventListener('change', (e) => {
-    e.preventDefault();
-    datosIngreso.nombreEmpresa = e.target.value;
-    console.log(datosIngreso);
-})
 
-
-accion.addEventListener('change', (e) => {
-    e.preventDefault();
-    datosIngreso.accion = e.target.value;
-    console.log(datosIngreso);
-})
-
-email.addEventListener('input', (e) => {
-    e.preventDefault();
-    datosIngreso.correo = e.target.value;
-    console.log(datosIngreso);
-})
-
-
-password.addEventListener('blur', (e) => {
-    e.preventDefault();
-    datosIngreso.password = e.target.value;
-    console.log(datosIngreso);
-})
-
-const datosIngreso = {
-    nombreEmpresa: '',
-    accion: '',
-    password: '',
-    correo: ''
-}
-
-
-
-
-
-function llenarSelect(arr) {
-    arr.forEach(empresa => {
-        const option = document.createElement('option')
-        option.value = empresa.nombre;
-        option.innerText = empresa.nombre;
-        console.log(option.value);
-        document.querySelector('#empresa').appendChild(option);
-    });
-    
-}
 
 
 
